5744f65b0399166a8b37fda6d33261fa1289f096
Sultanov Mirjamol <9625526+cljamal@users.noreply.github.com>
2023-06-13 15:04:07 +0500

    Update

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,27 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {defineNuxtModule, addPlugin, createResolver} from '@nuxt/kit'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+}
 
 export default defineNuxtModule<ModuleOptions>({
-  meta: {
-    name: 'my-module',
-    configKey: 'myModule'
-  },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+	meta: {
+		name: 'ray-themes',
+		configKey: 'rayThemes',
+		compatibility: {
+			// Semver version of supported nuxt versions
+			nuxt: '^3.0.0'
+		}
+	},
+	// Default configuration options of the Nuxt module
+	defaults: {
+		themesDir: 'themes',
+		theme: 'uiphoria2306'
+	},
+	setup(options, nuxt) {
+		const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
+		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+		addPlugin(resolver.resolve('./runtime/plugin'))
+	}
 })
